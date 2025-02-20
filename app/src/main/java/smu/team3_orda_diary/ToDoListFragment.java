785789576883197358315b49a0fa32a78ea17b0c
Yujin Jeong <yujin45@sookmyung.ac.kr>
2025-02-21 00:11:13 +0900

    [Refactor] Migrate TodolistActivity to ToDoListFragment - 기존 Activity 기반 일정 관리 기능을 Fragment로 이동 - ViewBinding을 적용하여 findViewById() 제거 - 하드코딩된 String을 strings.xml에서 관리하도록 변경 - 하드코딩된 Color를 colors.xml에서 관리하도록 변경 - UI 크기 대응 개선

--- app/src/main/java/smu/team3_orda_diary/ToDoListFragment.java
+++ app/src/main/java/smu/team3_orda_diary/ToDoListFragment.java
@@ -1,64 +1,119 @@
 package smu.team3_orda_diary;
 
+import android.app.Dialog;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ToDoListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Locale;
+
+import smu.team3_orda_diary.databinding.FragmentToDoListBinding;
+
 public class ToDoListFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private FragmentToDoListBinding binding;
+    private DBHelper dbHelper;
+    private TodoCustomAdapter todoAdapter;
+    private ArrayList<TodoItem> todoItems;
+    private String currentTime;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final String DATE_PATTERN_DB = "yyyy-MM-dd";
+    private static final String DATE_PATTERN_UI = "yyyy년 MM월 dd일";
+    private static final String CURRENT_TIME_FORMAT = "%d-%02d-%02d";
+    private static final String TODO_TODAY_FORMAT = "%d년 %d월 %d일";
 
-    public ToDoListFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ToDoListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ToDoListFragment newInstance(String param1, String param2) {
-        ToDoListFragment fragment = new ToDoListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentToDoListBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        dbHelper = new DBHelper(requireContext());
+        todoItems = new ArrayList<>();
+        todoAdapter = new TodoCustomAdapter(todoItems, requireContext());
+
+        binding.rvTodo.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.rvTodo.setAdapter(todoAdapter);
+
+        DateTimeFormatter dbFormatter = DateTimeFormatter.ofPattern(DATE_PATTERN_DB, Locale.getDefault());
+        DateTimeFormatter uiFormatter = DateTimeFormatter.ofPattern(DATE_PATTERN_UI, Locale.getDefault());
+
+        LocalDate now = LocalDate.now();
+        currentTime = now.format(dbFormatter);
+        binding.todoToday.setText(now.format(uiFormatter));
+
+        binding.todoCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfMonth) {
+                month += 1;
+                currentTime = String.format(Locale.getDefault(), CURRENT_TIME_FORMAT, year, month, dayOfMonth);
+                binding.todoToday.setText(String.format(Locale.getDefault(), TODO_TODAY_FORMAT, year, month, dayOfMonth));
+                loadRecentDB(currentTime);
+            }
+        });
+
+        binding.todoBtnWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAddTodoDialog();
+            }
+        });
+
+        loadRecentDB(currentTime);
+    }
+
+    private void showAddTodoDialog() {
+        Dialog dialog = new Dialog(requireContext(), android.R.style.Theme_Material_Light_Dialog);
+        dialog.setContentView(R.layout.dialog_edit_todo);
+
+        EditText etTitle = dialog.findViewById(R.id.et_title);
+        EditText etContent = dialog.findViewById(R.id.et_content);
+        Button btnOk = dialog.findViewById(R.id.todo_dialog_btn_ok);
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = etTitle.getText().toString();
+                String content = etContent.getText().toString();
+
+                dbHelper.insertTodo(title, content, currentTime);
+                dialog.dismiss();
+                Toast.makeText(requireContext(), getString(R.string.toast_todo_added), Toast.LENGTH_LONG).show();
+                loadRecentDB(currentTime);
+            }
+        });
+
+        dialog.show();
+    }
+
+    private void loadRecentDB(String writeDate) {
+        todoItems.clear();
+        todoItems.addAll(dbHelper.getTodoList(writeDate));
+        todoAdapter.notifyDataSetChanged();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_to_do_list, container, false);
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
