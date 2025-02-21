5a5bf5e84f46f1fcb15e5b8e8b4526d0e8a7ba23
Yujin Jeong <yujin45@sookmyung.ac.kr>
2025-02-21 14:54:33 +0900

    [Refactor] Migrate MapMemoActivity to MapMemoFragment - 기존 Activity 기반 지도 메모 기능을 Fragment로 이동 - SupportMapFragment를 동적으로 추가하도록 수정 - ViewBinding 적용하여 findViewById() 제거 - 하드코딩된 String을 strings.xml에서 관리하도록 변경 - UI ConstraintLayout 적용 및 레이아웃 구조 개선

--- app/src/main/java/smu/team3_orda_diary/ui/mapmemo/MapMemoFragment.java
+++ app/src/main/java/smu/team3_orda_diary/ui/mapmemo/MapMemoFragment.java
@@ -1,66 +1,121 @@
 package smu.team3_orda_diary.ui.mapmemo;
 
+import android.app.Dialog;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 import smu.team3_orda_diary.R;
+import smu.team3_orda_diary.databinding.DialogEditMapBinding;
+import smu.team3_orda_diary.databinding.FragmentMapMemoBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MapMemoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class MapMemoFragment extends Fragment {
+public class MapMemoFragment extends Fragment implements OnMapReadyCallback {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private FragmentMapMemoBinding binding;
+    private GoogleMap mMap;
+    private Geocoder geocoder;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MapMemoFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MapMemoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MapMemoFragment newInstance(String param1, String param2) {
-        MapMemoFragment fragment = new MapMemoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentMapMemoBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        SupportMapFragment mapFragment = new SupportMapFragment();
+        getChildFragmentManager().beginTransaction()
+                .replace(R.id.map_container, mapFragment)
+                .commit();
+
+        mapFragment.getMapAsync(this);
+
+        binding.mapBtnSearch.setOnClickListener(v -> searchLocation());
+        binding.mapBtnMark.setOnClickListener(v -> showAddMarkerDialog());
+    }
+
+    private void searchLocation() {
+        String searchResult = binding.mapEtPlace.getText().toString();
+        List<Address> addressList;
+
+        try {
+            geocoder = new Geocoder(requireContext());
+            addressList = geocoder.getFromLocationName(searchResult, 10);
+
+            if (addressList == null || addressList.isEmpty()) {
+                Toast.makeText(requireContext(), getString(R.string.map_no_results), Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Address address = addressList.get(0);
+            LatLng point = new LatLng(address.getLatitude(), address.getLongitude());
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(point, 15));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(requireContext(), getString(R.string.map_address_error), Toast.LENGTH_SHORT).show();
         }
     }
 
+    private void showAddMarkerDialog() {
+        Dialog dialog = new Dialog(requireContext(), android.R.style.Theme_Material_Light_Dialog);
+        DialogEditMapBinding dialogBinding = DialogEditMapBinding.inflate(getLayoutInflater());
+        dialog.setContentView(dialogBinding.getRoot());
+
+        dialogBinding.mapDialogBtnOk.setOnClickListener(v -> {
+            String title = dialogBinding.mapDialogTitle.getText().toString();
+            String content = dialogBinding.mapDialogContent.getText().toString();
+
+            if (mMap != null) {
+                LatLng currentPosition = mMap.getCameraPosition().target;
+                MarkerOptions markerOptions = new MarkerOptions()
+                        .title(title)
+                        .snippet(content)
+                        .position(currentPosition);
+
+                mMap.addMarker(markerOptions);
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentPosition, 15));
+                Toast.makeText(requireContext(), getString(R.string.map_marker_added), Toast.LENGTH_LONG).show();
+            }
+
+            dialog.dismiss();
+        });
+
+        dialog.show();
+    }
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map_memo, container, false);
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng SEOUL = new LatLng(37.56, 126.97);
+        mMap.addMarker(new MarkerOptions().position(SEOUL).title("서울").snippet("한국의 수도"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 10));
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
